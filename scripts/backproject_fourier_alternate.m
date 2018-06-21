c8687b0c934806b2debc0b7a4aba87941e17eddf
Arunabh <arunabhghosh98@gmail.com>
2018-06-21 10:49:13 +0200

    Remove interpolation for now

--- scripts/backproject_fourier_alternate.m
+++ scripts/backproject_fourier_alternate.m
@@ -45,6 +45,6 @@ function fourier_radial=backproject_fourier_alternate(f_p, prj_angles)
 	fourier_radial(index_non_nan) = ...
 		fourier_radial(index_non_nan)./count_matrix(index_non_nan);
 
-	fourier_radial(fourier_radial == 0) = NaN;
-	fourier_radial = inpaint_nans(fourier_radial, 2);
+	% fourier_radial(fourier_radial == 0) = NaN;
+	% fourier_radial = inpaint_nans(fourier_radial, 2);
 end
